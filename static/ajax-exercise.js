6e3a6bfcdefa05fae3cb9ad6e00fa9d3973344c4
Wooyang Son <sonwy102@gmail.com>
2020-10-23 02:09:56 +0000

    add orderMelons

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -16,7 +16,6 @@ $('#get-fortune-button').on('click', showFortune);
 
 
 
-
 // PART 2: SHOW WEATHER
 
 function showWeather(evt) {
@@ -44,8 +43,23 @@ function orderMelons(evt) {
 
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+    let formData = {
+      "qty": $("#qty-field").val(),
+      "melon_type": $("#melon-type-field").val()
+    };
+
+    $.post("/order-melons.json", formData, (response) => {
+      if (response.code == 'OK') {
+          $("#order-status").removeClass("order-error"); 
+          $("#order-status").html(`<p> ${response.msg} </p>`);
+      }
+      else {
+        $("#order-status").addClass("order-error");
+        $("#order-status").html(`<p> ${response.msg} </p>`);
+      };          
+    });
 }
 
-$("#order-form").on('submit', orderMelons);
+$("#order-form").on("submit", orderMelons);
 
 
